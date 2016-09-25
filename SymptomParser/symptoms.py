2fb1cdead7f2cb51f1fe2b0d2d7d0c6415d18a8a
Mayank Makwana <mayankmmmx@gmail.com>
2016-09-25 02:20:49 -0400

    fixed punc

--- SymptomParser/symptoms.py
+++ SymptomParser/symptoms.py
@@ -1,5 +1,6 @@
 import requests
 import pymysql.cursors
+import string
 
 POST_URL = "https://api.infermedica.com/v2/symptoms"
 header = { "app_id" : "9cfcafae", "app_key" : "55b78a933718c8e68ba37f2f8d80b1a7"}
@@ -18,8 +19,9 @@ try:
     with connection.cursor() as cursor:
         # Create a new record
         for key in json:
-			sql = "INSERT INTO `symptoms` (`symptom`, `id`) VALUES (%s, %s)"
-			cursor.execute(sql, (key['name'].lower(), key['id']))
+            exclude = set(string.punctuation)
+            sql = "INSERT INTO `symptoms` (`symptom`, `id`) VALUES (%s, %s)"
+            cursor.execute(sql, (''.join(ch for ch in key['name'].lower() if ch not in exclude), key['id']))
 
     # connection is not autocommit by default. So you must commit to save
     # your changes.
